b736a9678e5cefce04c2f8fcf98b7bc2a543b1d3
nhkim <nhkim1529@gmail.com>
2023-10-11 16:15:49 +0900

    Resume sample code

---
+++ Resume/app/src/main/java/com/nhkim/resume/MainActivity.kt
@@ -0,0 +1,167 @@
+package com.nhkim.resume
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.nhkim.resume.ui.theme.Pink80
+import com.nhkim.resume.ui.theme.ResumeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ResumeTheme {
+                MyResume()
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MyResume(){
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text="Android Dev Resume") }
+            )
+        }
+    ) { paddingValues ->
+        MyResumeContent(paddingValues)
+    }
+}
+
+@Composable
+fun MyResumeContent(paddingValues: PaddingValues){
+
+    val context = LocalContext.current
+
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)
+            .padding(20.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 10.dp),
+        shape = RoundedCornerShape(30.dp)
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(20.dp)
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.profile),
+                contentDescription = "profile",
+                modifier = Modifier
+                    .size(150.dp)
+                    .clip(CircleShape)
+                    .align(Alignment.CenterHorizontally),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "자기소개",
+                fontSize = 20.sp,
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "안녕하세요 저는 개발자입니다. 새로운 기술을 배우는 것을 좋아하고, 언제나 개발에 열정을 가지고 있습니다.",
+                fontSize = 15.sp,
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            Text(
+                text = "졸업한지 이제 1년이 다 되어가는데 얼른 취업하고 싶습니다!",
+                fontSize = 10.sp,
+                color = Pink80
+            )
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Divider(color = Color.Gray, thickness = 1.dp)
+
+            Spacer(modifier = Modifier.height(10.dp))
+
+            Text(
+                text = "핸드폰 번호 : 010-1234-5678",
+                fontSize = 15.sp,
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+
+            Text(
+                text = "이메일 : abc@naver.com",
+                fontSize = 15.sp,
+                modifier = Modifier
+                    .padding(10.dp)
+            )
+
+            Button(
+                onClick = {
+                val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:01012345678"))
+                    context.startActivity(intent)
+
+            },
+                modifier = Modifier.fillMaxWidth()) {
+                Text(text = "전화걸기")
+            }
+
+            Button(
+                onClick = {
+                    val intent = Intent(Intent.ACTION_SENDTO).apply {
+                        data = Uri.parse("mailto:abc@naver.com")
+                    }
+                    context.startActivity(intent)
+                },
+                modifier = Modifier.fillMaxWidth()) {
+                Text(text = "이메일 보내기")
+            }
+
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ResumeTheme {
+        MyResume()
+    }
+}

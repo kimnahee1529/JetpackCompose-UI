5684419a6f262f08bac22e0699fc6e0890880a6a
nhkim <nhkim1529@gmail.com>
2023-10-24 23:12:23 +0900

    TextStyle example code

---
+++ TextStyle/app/src/main/java/com/nhkim/textstyle/ui/theme/Type.kt
@@ -0,0 +1,39 @@
+package com.nhkim.textstyle.ui.theme
+
+import androidx.compose.material3.Typography
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+// Set of Material typography styles to start with
+val Typography = Typography(
+    titleLarge = TextStyle(
+        fontSize = 30.sp,
+        fontWeight = FontWeight.ExtraBold
+    ),
+    titleMedium = TextStyle(
+        fontSize = 20.sp,
+        fontWeight = FontWeight.Bold
+    ),
+    titleSmall = TextStyle(
+        fontSize = 10.sp,
+        fontWeight = FontWeight.SemiBold
+    )
+    /* Other default text styles to override
+    titleLarge = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 22.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.sp
+    ),
+    labelSmall = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Medium,
+        fontSize = 11.sp,
+        lineHeight = 16.sp,
+        letterSpacing = 0.5.sp
+    )
+    */
+)

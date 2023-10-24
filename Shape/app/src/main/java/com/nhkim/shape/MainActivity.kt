32c304c45d9e3e03bda6b27f9480f817ff293cac
nhkim <nhkim1529@gmail.com>
2023-10-25 00:14:57 +0900

    Shape example code

---
+++ Shape/app/src/main/java/com/nhkim/shape/MainActivity.kt
@@ -0,0 +1,131 @@
+package com.nhkim.shape
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.nhkim.shape.ui.theme.ShapeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RadiusTest2()
+        }
+    }
+}
+
+@Composable
+fun RadiusTest1(){
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp)
+                .clip(MaterialTheme.shapes.extraLarge)
+                .background(Color.Red)
+        ){
+
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp)
+                .clip(MaterialTheme.shapes.large)
+                .background(Color.Red)
+        ){
+
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp)
+                .clip(MaterialTheme.shapes.medium)
+                .background(Color.Red)
+        ){
+
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp)
+                .clip(MaterialTheme.shapes.small)
+                .background(Color.Red)
+        ){
+
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp)
+                .clip(MaterialTheme.shapes.extraSmall)
+                .background(Color.Red)
+        ){
+
+        }
+    }
+}
+
+@Composable
+fun RadiusTest2() {
+    Column {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp)
+                .clip(MaterialTheme.shapes.extraLarge)
+                .background(Color.Blue)
+        ) {
+
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp)
+                .clip(MaterialTheme.shapes.large)
+                .background(Color.Blue)
+        ) {
+
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(20.dp)
+                .clip(MaterialTheme.shapes.medium)
+                .background(Color.Blue)
+        ) {
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ShapeTheme {
+        RadiusTest2()
+    }
+}
---
+++ Shape/app/src/main/java/com/nhkim/shape/ui/theme/Shape.kt
@@ -0,0 +1,11 @@
+package com.nhkim.shape.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val shapes = Shapes(
+    extraLarge = RoundedCornerShape(100.dp),
+    large = RoundedCornerShape(10.dp),
+    medium = RoundedCornerShape(50.dp),
+)
